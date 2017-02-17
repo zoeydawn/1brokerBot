896e5ec8ea3aaccd2e195f4210f86a7625ded7e4
Donovan <donovan6925@gmail.com>
2017-02-16 22:32:16 -0500

    added functionality

--- models/trade.js
+++ models/trade.js
@@ -10,16 +10,34 @@ function userData(cb) {
     });
 };
 
+function quotes(assets, cb) {
+  const assetStr = assets.join(',');
+  axios.get(`https://1broker.com/api/v2/market/quotes.php?token=${process.env.API_TOKEN}&pretty=1&symbols=${assetStr}`)
+    .then((responce) => {
+      return cb(null, responce.data);
+    })
+    .catch(err => {
+      return cb(err);
+    });
+};
+
+function marketDetails(symbol, cb) {
+  axios.get(`https://1broker.com/api/v2/market/details.php?token=${process.env.API_TOKEN}&pretty=1&symbol=${symbol}`)
+    .then((responce) => {
+      return cb(null, responce.data);
+    })
+    .catch(err => {
+      return cb(err);
+    });
+};
+
 function placeOrder(options, cb) {
   const { symbol, margin, direction, leverage, type } = options;
   axios.get(`https://1broker.com/api/v2/order/create.php?token=${process.env.API_TOKEN}&pretty=1&symbol=${symbol}&margin=${margin}&direction=${direction}&leverage=${leverage}&order_type=${type}`)
     .then((responce) => {
-      // console.log('responce.data:', responce.data);
-      // res.send(responce.data);
       return cb(null, responce.data);
     })
     .catch(err => {
-      // res.status(400).send(err)
       return cb(err);
     });
 };
@@ -27,16 +45,27 @@ function placeOrder(options, cb) {
 function cancelOrder(id, cb) {
   axios.get(`https://1broker.com/api/v2/position/close.php?token=${process.env.API_TOKEN}&pretty=1&position_id=${id}`)
   .then((responce) => {
-    // console.log('responce.data:', responce.data);
-    // res.send(responce.data);
     return cb(null, responce.data);
   })
   .catch(err => {
-    // res.status(400).send(err)
     return cb(err);
   });
-}
+};
+
+function editPosition(options, cb) {
+  const { id, stopLoss, takeProfit } = options;
+  axios.get(`https://1broker.com/api/v2/position/edit.php?token=${process.env.API_TOKEN}&pretty=1&position_id=${id}&stop_loss=${stopLoss}&take_profit=${takeProfit}&trailing_stop_loss=true`)
+  .then((responce) => {
+    return cb(null, responce.data);
+  })
+  .catch(err => {
+    return cb(err);
+  });
+};
 
 module.exports.userData = userData;
+module.exports.quotes = quotes;
 module.exports.placeOrder = placeOrder;
 module.exports.cancelOrder = cancelOrder;
+module.exports.editPosition = editPosition;
+module.exports.marketDetails = marketDetails;
--- test.js
+++ test.js
@@ -1,38 +1,118 @@
 require('dotenv').config({ silent: true });
 
-const { userData, placeOrder, cancelOrder } = require('./models/trade');
+const {
+  userData,
+  placeOrder,
+  cancelOrder,
+  editPosition,
+  quotes,
+  marketDetails,
+} = require('./models/trade');
 
 // ----------- // GET USER DATA // ----------- //
-userData((err, data) => {
-  if(err) {
-    console.log('ERROR:', err);
-    return;
-  }
-  return console.log(data.response);
-});
+function getUserData() {
+  userData((err, data) => {
+    if(err) {
+      console.log('ERROR:', err);
+      return;
+    }
+    return console.log(data.response);
+  });
+};
+
+// ----------- // GET QUOTES // ----------- //
+function getQuotes(arr) {
+  quotes(arr, (err, data) => {
+    if(err) {
+      console.log('ERROR:', err);
+      return;
+    }
+    return console.log(data.response);
+  });
+};
+
+// ----------- // GET MARKET DETAILS // ----------- //
+function details(symbol) {
+  marketDetails(symbol, (err, data) => {
+    if(err) {
+      console.log('ERROR:', err);
+      return;
+    }
+    return console.log(data.response);
+  });
+};
 
 // ----------- // PLACE ORDER // ----------- //
-// const options = {
+function placeNewOrder(options) {
+  placeOrder(options, (err, data) => {
+    if(err) {
+      console.log('ERROR:', err);
+      return;
+    }
+    return console.log(data);
+  });
+};
+
+// ----------- // EDIT ORDER // ----------- //
+function edit(options) {
+  editPosition(options, (err, data) => {
+    if(err) {
+      console.log('ERROR:', err);
+      return;
+    }
+    return console.log(data);
+  });
+};
+
+// ----------- // CANCEL ORDER // ----------- //
+function cancel(id) {
+  cancelOrder(id, (err, data) => {
+    if(err) {
+      console.log('ERROR:', err);
+      return;
+    }
+    return console.log(data.response);
+  });
+};
+
+// getUserData();
+
+// getQuotes(['USDJPY', 'GOLD']);
+
+// details('USDJPY');
+
+// const newOrderOptions = {
 //   symbol: 'USDJPY',
 //   margin: 0.02,
-//   direction: 'short',
+//   direction: 'long',
 //   leverage: '1',
 //   type: 'market',
 // };
-// placeOrder(options, (err, data) => {
-//   if(err) {
-//     console.log('ERROR:', err);
-//     return;
-//   }
-//   return console.log(data);
-// });
+// placeNewOrder(newOrderOptions);
 
-// ----------- // CANCEL ORDER // ----------- //
-// const id = 1076532;
-// cancelOrder(id, (err, data) => {
-//   if(err) {
-//     console.log('ERROR:', err);
-//     return;
-//   }
-//   return console.log(data.response);
-// });
+// const editOptions = {
+//   id: 1076567,
+//   stopLoss: 200,
+//   takeProfit: 75,
+// }
+// edit(editOptions);
+
+
+function go() {
+  userData((err, data) => {
+    if(err) {
+      console.log('ERROR:', err);
+    } else {
+      console.log(data.response);
+      // console.log(data.response.positions_open.length);
+      if (!data.response.positions_open.length) {
+        console.log('TRUE');
+      }
+    }
+  });
+  setTimeout(() => {
+    go();
+  }, 30000)
+};
+
+// go();
