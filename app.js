1ce738c9121985a34e031446ff5d951880de3051
Donovan <donovan6925@gmail.com>
2017-02-16 22:44:33 -0500

    removed express server because it was unnecessary

--- app.js
+++ app.js
@@ -1,12 +1,33 @@
 require('dotenv').config({ silent: true });
-const express = require('express');
-const app = express();
 
-const PORT = process.env.PORT || 8080;
-const api = require('./routes/api');
+const {
+  userData,
+  placeOrder,
+  cancelOrder,
+  editPosition,
+  quotes,
+  marketDetails,
+} = require('./models/trade');
 
-app.use('/api', api);
+function trade() {
+  userData((err, data) => {
+    if(err) {
+      console.log('ERROR:', err);
+    } else {
+      const { positions_open, orders_open } = data.responce;
+      console.log(data.response);
+      // console.log(data.response.positions_open.length);
+      if (!positions_open.length && !orders_open.length) {
+        console.log('NO OPEN POSITIONS OR ORDERS');
+      } else {
+        console.log(`${positions_open.length} positions open`);
+        console.log(`${orders_open.length} orders open`);
+      }
+    }
+  });
+  setTimeout(() => {
+    trade();
+  }, 20000)
+};
 
-app.listen(PORT, function () {
-  console.log(`Express listinging on port ${PORT}`);
-})
+trade();
